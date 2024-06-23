160daf454b42a9b1fd72b5512524b019325e1003
DangTH <dangth@ints.vn>
2024-06-23 21:30:27 +0700

    add code

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -12,25 +12,23 @@ const storage = multer.diskStorage({
   },
 });
 
-const uploadVideo = multer({ storage: storage });
+const uploadVideo = multer({ storage: storage, limits: { fileSize: 100 * 1024 * 1024 } });
 
 // Cấu hình AWS SDK
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
-
 
 // Cấu hình Multer
 const uploadImage = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 100 * 1024 * 1024 } // Giới hạn kích thước file 5MB
+  limits: { fileSize: 100 * 1024 * 1024 }, // Giới hạn kích thước file 5MB
 });
-
 
 module.exports = {
   uploadImage,
   uploadVideo,
   s3,
-}
+};
